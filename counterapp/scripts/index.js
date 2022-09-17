693f2ea844af029a56643d2eca26c5b38f504903
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-16 23:44:05 -0500

    FEAT: save function operates on click, store the daved information next

--- counterapp/scripts/index.js
+++ counterapp/scripts/index.js
@@ -5,10 +5,9 @@ let countEl = document.getElementById('count-el');
 function increment() {
   count += 1;
   countEl.innerText = count;
-  console.log(`count has been clicked this many times ${count}`);
 }
 
 // 1. create a function, save(), which logs out the count when it called
 function save() {
-  console.log('save button clicked');
+  console.log(`this is the count: ${count}`);
 }
