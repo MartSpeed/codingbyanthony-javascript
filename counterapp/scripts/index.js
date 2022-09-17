a9527e0e5caeda1df51bbcdaf38a15cb67fb90a7
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-16 23:40:54 -0500

    FEAT: save button cliced and responds to html console

--- counterapp/scripts/index.js
+++ counterapp/scripts/index.js
@@ -1,13 +1,14 @@
-// document.getElementById('count-el').innerText = 5;
-
 // store the count
 let count = 0;
 let countEl = document.getElementById('count-el');
-
-// change the count-el in the HTML to reflect the new count
 
 function increment() {
   count += 1;
   countEl.innerText = count;
   console.log(`count has been clicked this many times ${count}`);
+}
+
+// 1. create a function, save(), which logs out the count when it called
+function save() {
+  console.log('save button clicked');
 }
