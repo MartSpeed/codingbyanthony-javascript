9632791755ea7c7fe69276151451e8ba369beee7
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-17 22:08:05 -0500

    FEAT: set count to 0 and innertext to 0 in the save function

--- counterapp/scripts/index.js
+++ counterapp/scripts/index.js
@@ -1,4 +1,3 @@
-// 1. grab the save-el paragraph and store it in a variable called saveEl
 let count = 0;
 let countEl = document.getElementById('count-el');
 let saveEl = document.getElementById('save-el');
@@ -9,10 +8,8 @@ function increment() {
 }
 
 function save() {
-  // 2. create a variable that contains both the count and the dash separator, i.e. "12 - "
   let _save = ' ' + count + ' - ';
-  // 3. render the variable in the saveEl using innerText
   saveEl.innerText += _save;
-  // NB: make sure to not delete the existing content of the paragraph
-  console.log(`this is the count: ${count}`);
+  countEl.innerText = 0;
+  count = 0;
 }
