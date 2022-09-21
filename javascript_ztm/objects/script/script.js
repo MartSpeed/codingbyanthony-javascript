fc19a7785f6462958528a15b1666c2dfe295767a
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-21 09:44:52 -0500

    FEAT: added success and error messages to the dom

--- javascript_ztm/objects/script/script.js
+++ javascript_ztm/objects/script/script.js
@@ -43,13 +43,16 @@ let newsfeed = [
 
 let userNamePrompt = prompt('Whats your username');
 let passwordPrompt = prompt('whats your password');
+let bobbyEl = document.getElementById('bobby-el');
+let sallyEl = document.getElementById('sally-el');
+let errorEl = document.getElementById('error-el');
 
 function signIn(username, password) {
   if (username === database[0].username && password === database[0].password) {
-    console.log(newsfeed[0].username + ' says: ' + newsfeed[0].timeline);
-    console.log(newsfeed[1].username + ' says: ' + newsfeed[1].timeline);
+    bobbyEl.innerText = newsfeed[0].username + ' says: ' + newsfeed[0].timeline;
+    sallyEl.innerText = newsfeed[1].username + ' says: ' + newsfeed[1].timeline;
   } else {
-    alert('sorry, wrong username and password');
+    errorEl.innerText = 'sorry, wrong username and password';
   }
 }
 signIn(userNamePrompt, passwordPrompt);
