2512484b2134be953f0a1b94c3cb9fd513572a49
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-21 00:25:19 -0500

    FEAT: added a list of arrays and tagged them with the p element on the dom

---
+++ javascript_ztm/arrays/script/script.js
@@ -0,0 +1,12 @@
+// list
+let list = ['tiger', 'cat', 'bear', 'bird'];
+//element
+let anim1El = document.getElementById('animal1-el');
+let anim2El = document.getElementById('animal2-el');
+let anim3El = document.getElementById('animal3-el');
+let anim4El = document.getElementById('animal4-el');
+// dom input
+anim1El.innerText = list[0];
+anim2El.innerText = list[1];
+anim3El.innerText = list[2];
+anim4El.innerText = list[3];
