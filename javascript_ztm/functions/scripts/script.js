e75020c83e2faddecb06e5731e2a9c2fb5cf01de
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-20 22:42:55 -0500

    FEAT: call the function, not the onclick method

--- javascript_ztm/functions/scripts/script.js
+++ javascript_ztm/functions/scripts/script.js
@@ -1,5 +1,12 @@
 let greeting = document.getElementById('greeting-el');
-let message = 'Hello';
+let message = 'grand rising';
 function sayHello() {
   greeting.innerText = message;
 }
+sayHello();
+
+message = 'see you later';
+let farewell = function () {
+  document.getElementById('farewell-el').innerText = message;
+};
+farewell();
