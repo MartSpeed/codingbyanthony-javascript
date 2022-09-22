b819e93e87bc947e6776efc0df2d176191147a23
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-22 07:29:31 -0500

    FEAT: you can create functions seperately and call them using the for each method

--- javascript_ztm/loops/scripts/script.js
+++ javascript_ztm/loops/scripts/script.js
@@ -17,30 +17,38 @@ let todos = [
 ];
 
 for (let i = 0; i < todos.length; i++) {
-  forLoopEl.innerText += `${todos[i]}!\n`;
+  forLoopEl.innerText += `for loop: ${todos[i]}!\n`;
 }
 
 // while loops
 let whileLoopEl = document.getElementById('whileLoop-el');
 let counterOne = 0;
 while (counterOne < 10) {
-  whileLoopEl.innerText += `[${counterOne}],`;
+  whileLoopEl.innerText += `while loop: ${counterOne},\n`;
   counterOne++;
 }
 
 // while loop in reverse
 counterOne = 10;
 while (counterOne > 0) {
-  whileLoopEl.innerText += '\n' + counterOne + '!';
+  whileLoopEl.innerText += `'\nwhile loop: ${counterOne}!`;
   counterOne--;
 }
 
 // do while loop
 let doLoopEl = document.getElementById('doLoop-el');
 do {
-  doLoopEl.innerText += `${counterOne}< `;
+  doLoopEl.innerText += `do while loop: ${counterOne}\n`;
   counterOne++;
 } while (counterOne < 10);
 
 // for each loop method
 let forEachEl = document.getElementById('forEach-el');
+todos.forEach(function (i) {
+  forEachEl.innerText += `forEach method: ${i}\n`;
+});
+
+function logTodos(todo, i) {
+  forEachEl.innerText += `calling log todo array list and index with for each loop: ${todo}, ${i}\n`;
+}
+todos.forEach(logTodos);
