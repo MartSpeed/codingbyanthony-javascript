6c6e79e347d5e0440672fbe6e4351c5e049b4231
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-22 06:52:56 -0500

    FEAT: when using document get id with for loops, use the += to show all elements

--- javascript_ztm/loops/scripts/script.js
+++ javascript_ztm/loops/scripts/script.js
@@ -1,2 +1,22 @@
-let jsEl = (document.getElementById('js-el').innerText =
-  'Javascript is working');
+let jsEl = document.getElementById('js-el');
+function jsRunning() {
+  jsEl.innerText = 'Javascript is running, and the function is invoked';
+}
+jsRunning();
+
+// for loop
+// adding an exclamation mark using loops
+let forLoopEl = document.getElementById('forLoop-el');
+
+let todos = [
+  'clean room',
+  'brush teeth',
+  'exercise',
+  'study javascript',
+  'eat healthy',
+];
+
+for (let i = 0; i < todos.length; i++) {
+  forLoopEl.innerText += `${todos[i]}\n`;
+  console.log(todos[i]);
+}
