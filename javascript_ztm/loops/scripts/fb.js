3b528b939a1f25695d9e7b80f8fc3ac6cff62a3d
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-22 07:42:05 -0500

    FEAT: added all elements and calls for the new information for the for loop database loop and function

--- javascript_ztm/loops/scripts/fb.js
+++ javascript_ztm/loops/scripts/fb.js
@@ -3,6 +3,14 @@ let database = [
     username: 'Anthony',
     password: 'supersecret',
   },
+  {
+    username: 'Flash',
+    password: '1234',
+  },
+  {
+    username: 'TheMainMan',
+    password: 'boomstick',
+  },
 ];
 
 let newsfeed = [
@@ -14,12 +22,17 @@ let newsfeed = [
     username: 'sally',
     timeline: 'javascript is so cool',
   },
+  {
+    username: 'mitch',
+    timeline: 'javascript is pretty cool',
+  },
 ];
 
 let userNamePrompt = prompt('Whats your username');
 let passwordPrompt = prompt('whats your password');
 let bobbyEl = document.getElementById('bobby-el');
 let sallyEl = document.getElementById('sally-el');
+let mitchEl = document.getElementById('mitch-el');
 let errorEl = document.getElementById('error-el');
 
 function signIn(username, password) {
