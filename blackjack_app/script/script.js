ec143aabec38d60e1040be35e82baf7717c1d993
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-26 23:37:25 -0500

    CHORE: fixed some element values and placement

--- blackjack_app/script/script.js
+++ blackjack_app/script/script.js
@@ -4,24 +4,24 @@ let jsTestEl = (document.getElementById('js-test-el').innerText =
 
 let firstCard = 11;
 let secondCard = 7;
-let firstCardEl = (document.getElementById(
-  'firstCard-el'
-).innerText = `first card: ${firstCard}`);
-let secondCardEl = (document.getElementById(
-  'secondCard-el'
-).innerText = `second card: ${secondCard}`);
+let firstCardEl = (document.getElementById('firstCard-el').innerText =
+  firstCard + ', ');
 
-let sum = firstCard + secondCard + 3;
-let sumEl = (document.getElementById('sum-el').innerText = `Sum: ${sum}`);
+let secondCardEl = (document.getElementById('secondCard-el').innerText =
+  secondCard);
+
+let sum = firstCard + secondCard;
+// let sumEl = (document.getElementById('sum-el').innerText = `Sum: ${sum}`);
+let sumEl = (document.querySelector('#sum-el').innerText = `Sum: ${sum}`);
 let hasBlackJack = false;
 // create a variable called isAlive and assign it to true
 // flip its value to false in the appropriate code block
 let isAlive = true;
 
-let resultEl = document.getElementById('result-el');
+let messageEl = document.getElementById('message-el');
 const message = [
   'Do you want to draw a new card?',
-  "Woohoo!, You've got Blackjack!",
+  "You've got Blackjack!",
   "You've busted, would you like to play again?",
 ];
 
@@ -30,12 +30,12 @@ const message = [
 function startGame() {
   // logic for blackjack
   if (sum <= 20) {
-    resultEl.innerText = message[0];
+    return (messageEl.innerText = message[0]);
   } else if (sum === 21) {
-    resultEl.innerText = message[1];
     hasBlackJack = true;
+    return (messageEl.innerText = message[1]);
   } else {
-    resultEl.innerText = message[2];
     isAlive = false;
+    return (messageEl.innerText = message[2]);
   }
 }
