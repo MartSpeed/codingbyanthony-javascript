1b7e358dfd8d73e00669640f2721af9d7eb1a6e2
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-29 23:05:00 -0500

    CHORE: refactore let and const variables

--- chrome_extension/README.md
+++ chrome_extension/README.md
@@ -3,4 +3,6 @@
 1. - [x] add button and input tag
 2. - [x] style the button and input tag
 3. - [x] make the input button work with onclick
-4. - [] refactor to the addEventListener
+4. - [x] refactor to the addEventListener
+5. - [x] create the myLeads array and inputEl
+6. - [] push to the myLeads array
--- chrome_extension/scripts/index.js
+++ chrome_extension/scripts/index.js
@@ -1,9 +1,13 @@
 let testEl = document.querySelector('#jstest-el');
 testEl.textContent = 'JS is running';
+
 let logEl = document.querySelector('#log-el');
+const inputEl = document.querySelector('#input-el');
+
+let myLeads = [];
 
 //log out "button clicked" when the user clicks the save input
-let inputBtn = document.querySelector('#input-btn');
+const inputBtn = document.querySelector('#input-btn');
 inputBtn.addEventListener('click', function () {
-  logEl.textContent += 'Button Clicked from addEventListener! ';
+  logEl.textContent += 'Button Clicked! ';
 });
