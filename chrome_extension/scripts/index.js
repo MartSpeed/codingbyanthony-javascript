80a2839dbf0d61022dc00725a3fd785f44b91136
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-29 23:28:42 -0500

    FEAT: input value logged into the array test success

--- chrome_extension/scripts/index.js
+++ chrome_extension/scripts/index.js
@@ -2,6 +2,7 @@ let testEl = document.querySelector('#jstest-el');
 testEl.textContent = 'JS is running';
 
 let logEl = document.querySelector('#log-el');
+let resultEl = document.querySelector('#result-el');
 const inputEl = document.querySelector('#input-el');
 
 let myLeads = [];
@@ -9,6 +10,7 @@ let myLeads = [];
 //log out "button clicked" when the user clicks the save input
 const inputBtn = document.querySelector('#input-btn');
 inputBtn.addEventListener('click', function () {
-  myLeads.push(' codingBYanthony.com ');
-  logEl.textContent += myLeads;
+  myLeads.push(inputEl.value);
+  logEl.textContent = myLeads;
+  resultEl.textContent += inputEl.value;
 });
