e78c20d636c2dc16af1c3e6e25ad3ef852a168c9
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-30 14:45:17 -0500

    FEAT: added new test ul elements and standard testing elements for inner html

--- chrome_extension/scripts/index.js
+++ chrome_extension/scripts/index.js
@@ -1,10 +1,14 @@
 const testEl = document.querySelector('#jstest-el');
 testEl.textContent = 'JS is running';
 
-const logEl = document.querySelector('#log-el');
+// standard input
 const resultEl = document.querySelector('#result-el');
 const inputEl = document.querySelector('#input-el');
 const ulEl = document.querySelector('#ul-el');
+
+// test scripts
+const logEl = document.querySelector('#log-el');
+const testUlEl = document.querySelector('#test-ul');
 
 let myLeads = [];
 
@@ -12,10 +16,10 @@ let myLeads = [];
 const inputBtn = document.querySelector('#input-btn');
 inputBtn.addEventListener('click', function () {
   myLeads.push(inputEl.value);
-  ulEl.textContent += `${inputEl.value} `;
+  ulEl.innerHTML += `<li>${inputEl.value}</> `;
 
   // forloop for the leads
   for (let i = 0; i < myLeads.length; ++i) {
-    ulEl.textContent += myLeads[i];
+    testUlEl.innerHTML += `<li>${myLeads[i]}</li>`;
   }
 });
